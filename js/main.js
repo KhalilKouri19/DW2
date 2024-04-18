2df36ba6b32c956ca3c68f303d9ec72b5890586b
KhalilKouri19 <keacpipi45@gmail.com>
2024-04-18 19:39:31 -0300

    Primer desafío

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let nombreProducto = "Teclados";
+let precioUnitario = 1000;
+let cantidadDeseada = prompt(`Ingresar la cantidad desea de ${nombreProducto} que desee comprar:`)
+let costoTotal = precioUnitario*cantidadDeseada
+alert(`El costo total de su compra es: $${costoTotal}`)

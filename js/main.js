46a62e97da4976fe06afa7a6cf16cd98bccbabfd
KhalilKouri19 <keacpipi45@gmail.com>
2024-04-25 19:56:07 -0300

    Segundo Desafio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
+// PRIMER y SEGUNDO DESAFÍO:
+
 let nombreProducto = "Teclados";
 let precioUnitario = 1000;
 let cantidadDeseada = prompt(`Ingresar la cantidad desea de ${nombreProducto} que desee comprar:`)
 let costoTotal = precioUnitario*cantidadDeseada
-alert(`El costo total de su compra es: $${costoTotal}`)
+let descuento = 0;
+
+console.log(`Costo sin descuento de su compra: $${costoTotal}`)
+
+if(cantidadDeseada >= 5){
+    descuento = (costoTotal*10)/100
+}
+
+costoTotal -= descuento
+
+console.log(`Descuento: -$${descuento}`)
+console.log(`Costo total con descuento de su compra: $${costoTotal}`)
